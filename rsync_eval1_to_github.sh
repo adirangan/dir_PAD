7bbea027d4a75d93281653dfe2729350981aa73d
adirangan <adirangan@yahoo.com>
2022-10-07 04:57:48 -0400

    updating dir_PAD for shreya thirumalai

--- rsync_eval1_to_github.sh
+++ rsync_eval1_to_github.sh
@@ -19,5 +19,6 @@ git pull ;
 cd /home/rangan/dir_bcc/dir_PAD ;
 
 #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
-# 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
-# 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
+#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
+#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
+#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
