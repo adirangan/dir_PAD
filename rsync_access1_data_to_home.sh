04e822ce855fc69b162a3503b572217b678b3eaa
adirangan <adirangan@yahoo.com>
2022-10-11 02:35:41 -0400

    updating dir_PAD for shreya thirumalai

---
+++ rsync_access1_data_to_home.sh
@@ -0,0 +1,16 @@
+rsync -avum \
+      /data/rangan/dir_bcc/dir_PAD \
+      /home/rangan/dir_bcc/ \
+      --exclude="/*/*/*/*/" \
+      --include="*/" \
+      --include="*.sh" \
+      --include="*.m" \
+      --include="*.c" \
+      --include="*.h" \
+      --include="*.f" \
+      --include="*.tex" \
+      --include="*.bib" \
+      --include="*.pdf" \
+      --include="*.fig" \
+      --exclude="*" \
+      ;
