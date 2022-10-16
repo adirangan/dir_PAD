fa2dee15b70b8acbbbebef14413f75475257d4b5
adirangan <adirangan@yahoo.com>
2022-10-16 04:01:24 -0400

    updating dir_PAD for shreya thirumalai

--- rsync_eval1_to_github.sh
+++ rsync_eval1_to_github.sh
@@ -1,14 +1,14 @@
-rsync -avum \
-    rangan@access1.cims.nyu.edu:/data/rangan/dir_bcc/dir_PAD \
-    /home/rangan/dir_bcc/ \
-    --include="*/" \
-    --include="*.h" \
-    --include="*.c" \
-    --include="*.in" \
-    --include="*.make" \
-    --include="*.m" \
-    --include="*.sh" \
-    --exclude="*" ;
+#rsync -avum \
+#    rangan@access1.cims.nyu.edu:/data/rangan/dir_bcc/dir_PAD \
+#    /home/rangan/dir_bcc/ \
+#    --include="*/" \
+#    --include="*.h" \
+#    --include="*.c" \
+#    --include="*.in" \
+#    --include="*.make" \
+#    --include="*.m" \
+#    --include="*.sh" \
+#    --exclude="*" ;
 rsync -avum \
     /home/rangan/dir_bcc/dir_PAD \
     rangan@access1.cims.nyu.edu:/data/rangan/dir_bcc/ \
