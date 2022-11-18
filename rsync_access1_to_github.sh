adef5383080a1a208ea1eccd2b43dcf2f0e18799
Aaditya Rangan <rangan@math.nyu.edu>
2022-11-18 04:54:14 -0500

    updating dir_PAD for shreya thirumalai

--- rsync_access1_to_github.sh
+++ rsync_access1_to_github.sh
@@ -46,3 +46,4 @@ cd /data/rangan/dir_bcc/dir_PAD ;
 #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
 #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
 #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
+#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
